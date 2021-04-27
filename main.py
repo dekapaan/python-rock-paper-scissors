69c8991724263c9203a5f88891df9e6a892be118
dekapaan <terblancheronald@gmail.com>
2021-04-27 16:55:15 +0200

    Rock, Paper, Scissors game

---
+++ main.py
@@ -0,0 +1,27 @@
+play = "y"
+import random
+while play == "y":
+    print("...rock...")
+    print("...paper...")
+    print("...scissors...")
+    choices = ["rock", "paper", "scissors"]
+    comp_choice = random.choice(choices)
+    user_choice = input("Player choice: ")
+    print("Shoot!")
+    if comp_choice == user_choice:
+        print("Tie!")
+    elif comp_choice == "rock" and user_choice == "scissors":
+        print("You lose!")
+    elif comp_choice == "rock" and user_choice == "paper":
+        print("You win!")
+    elif comp_choice == "scissors" and user_choice == "rock":
+        print("You win!")
+    elif comp_choice == "scissors" and user_choice == "paper":
+        print("You lose!")
+    elif comp_choice == "paper" and user_choice == "rock":
+        print("You lose!")
+    elif comp_choice == "paper" and user_choice == "scissors":
+        print("You win!")
+    print("")
+    play = input("Another round?(y/n) ")
+    print("")
